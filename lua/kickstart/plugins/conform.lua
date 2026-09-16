2d5672fb6540891c58fc60ee237c40aab44efee0
Chirag Karia <chirag.karia@terrasense.ca>
2026-09-16 19:57:50 +0000

    Add FormatDisable and FormatEnable to lazy loading triggers for conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    cmd = { 'ConformInfo', 'FormatDisable', 'FormatEnable'},
     keys = {
       {
         '<leader>f',
